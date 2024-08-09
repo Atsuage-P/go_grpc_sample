f283115716c40a2d0043510585e723bac8208df6
atsuage <atsuage.programmer@gmail.com>
2024-08-09 17:31:50 +0900

    gRPCサーバ実装

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # gRPCのお試し
 ## 概要
 - [zennの記事](https://zenn.dev/hsaki/books/golang-grpc-starting) を参考
+
+## 動作確認
+- サーバ起動
+```
+go run main.go
+```
+
+- `gRPCurl` による動作確認
+```
+# サーバ内のサービス一覧表示
+grpcurl -plaintext localhost:8080 list
+
+# 特定のサービスのメソッド一覧表示
+grpcurl -plaintext localhost:8080 list <サービス名>
+grpcurl -plaintext localhost:8080 list myapp.GreetingService
+
+# メソッドの呼び出し
+grpcurl -plaintext -d '{"name": "user"}' localhost:8080 <メソッド名>
+grpcurl -plaintext -d '{"name": "user"}' localhost:8080 myapp.GreetingService.Hello
+```
---
+++ cmd/server/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	hellopb "mygrpc/pkg/grpc"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	// gRPCサーバ作成
+	s := grpc.NewServer()
+	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+
+	// gRPCurlでの検証用
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	// SIGINT(Ctrl+C)でグレースフルシャットダウンさせる
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
+
+type myServer struct {
+	// 前方互換性を保つために組み込んでおくおまじない
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	return &hellopb.HelloResponse{
+		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	}, nil
+}
